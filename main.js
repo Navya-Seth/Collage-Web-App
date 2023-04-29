7a07639eba06fdce49b0e601534ddb2bf5020348
Navya-Seth <91605093+Navya-Seth@users.noreply.github.com>
2023-04-29 15:07:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+var Content;
+var recognition = new SpeechRecognition();
+
+function start()
+{
+    recognition.start();
+}
+
+recognition.onresult = function(event) {
+     console.log(event);
+      Content = event.results[0][0].transcript.toLowerCase(); 
+      console.log(Content);
+       if (Content == "selfie") {
+         speak(); 
+        } 
+    }
+
+
+camera = document.getElementById("camera");
+Webcam.set({
+    width:500,
+    height:400,
+    image_format : 'jpeg',
+    jpeg_quality:90
+});
+
+function speak(){
+
+       
+    var synth = window.speechSynthesis;
+    Webcam.attach(camera);
+
+    setTimeout(function(){
+        img_id = "coding1.jpg";
+        take_snapshot();
+        speak_data = "Taking your selfie in 10 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    } ,5000);
+
+    speak_data = "Taking your next Selfie in 5 seconds";
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+
+    setTimeout(function(){
+        img_id = "coding2.jpg";
+        take_snapshot();
+        speak_data = "Taking your selfie in 10 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    } ,10000);
+
+    setTimeout(function(){
+        img_id = "coding3.jpg";
+        take_snapshot();
+        speak_data = "Taking your selfie in 10 seconds";
+        var utterThis = new SpeechSynthesisUtterance(speak_data);
+        synth.speak(utterThis);
+    } ,15000);
+
+
+};
+
+
+
